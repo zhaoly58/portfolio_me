169924b66fccf879715797af3803fb34dab51b96
Jimmy Zhao <jimmyzly88@gmail.com>
2025-03-28 14:49:09 +0900

    created Testimonials Screen basic UI framework

---
+++ lib/testimonials_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class TestimonialsScreen extends StatefulWidget {
+  const TestimonialsScreen({super.key});
+
+  @override
+  State<TestimonialsScreen> createState() => _TestimonialsScreenState();
+}
+
+class _TestimonialsScreenState extends State<TestimonialsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Testimonials",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF0077FF),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "The Trust From Clients",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    controller.previousPage(
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.linear,
+                    );
+                  },
+                  icon: Icon(Icons.arrow_back_sharp),
+                ),
+                IconButton(
+                  onPressed: () {
+                    controller.nextPage(
+                      duration: Duration(milliseconds: 200),
+                      curve: Curves.linear,
+                    );
+                  },
+                  icon: Icon(Icons.arrow_forward_sharp),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+
+            //TODO: Container
+            SizedBox(
+              height: 366,
+              child: PageView.builder(
+                // itemCount: serviceContent.length,
+                controller: controller,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  // return ServiceCard(
+                  //   title: "${serviceContent[index]["title"]}",
+                  //   subTitle: "${serviceContent[index]["subTitle"]}",
+                  // );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
